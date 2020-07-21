8a58cb4f49725109d1ed75d11dfdba0ffb22cde9
Igor <zakatov.igor@yandex.ru>
2020-07-21 22:49:09 +0300

    Added API Class

---
+++ src/api/index.js
@@ -0,0 +1,54 @@
+const BASE_URL = 'http://localhost:3001/'
+
+class API {
+  constructor(url, options = {}) {
+    this.url = url
+    this.options = options
+  }
+
+  _getUrl(url) {
+    let baseUrl = this.url
+
+    if (baseUrl[baseUrl.length - 1] === '/') {
+      baseUrl = baseUrl.slice(0, baseUrl.length - 1)
+    }
+
+    if (url[0] === '/') {
+      url = url.slice(1)
+    }
+
+    return `${baseUrl}/${url}`
+  }
+
+  get(url, options = {}) {
+    return fetch(this._getUrl(url), {
+      method: 'GET',
+      ...options,
+    })
+  }
+
+  post(url, data = {}, options = {}) {
+    return fetch(this._getUrl(url), {
+      method: 'POST',
+      body: JSON.stringify(data),
+      ...options,
+    })
+  }
+
+  put(url, data = {}, options = {}) {
+    return fetch(this._getUrl(url), {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      ...options,
+    })
+  }
+
+  delete(url, options = {}) {
+    return fetch(this._getUrl(url), {
+      method: 'DELETE',
+      ...options,
+    })
+  }
+}
+
+export default new API(BASE_URL)
