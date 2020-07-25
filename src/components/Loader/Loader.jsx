cb0f949702cab48fee5e4c6012f576b72e8b35bb
Igor <zakatov.igor@yandex.ru>
2020-07-25 18:48:28 +0300

    Added Loader customization

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import './Loader.sass'
 
-const Loader = (props) => {
+const Loader = ({ inline, size, borderWidth }) => {
   return (
-    <div className={'loader'}>
-      <div />
+    <div
+      className={'loader'}
+      style={{ display: inline ? 'inline-flex' : 'flex' }}
+    >
+      <div
+        style={{
+          width: size,
+          height: size,
+          borderWidth: `${borderWidth}px`,
+        }}
+      />
     </div>
   )
+}
+
+Loader.propTypes = {
+  inline: PropTypes.bool,
+  size: PropTypes.string,
+  borderWidth: PropTypes.number,
+}
+
+Loader.defaultProps = {
+  inline: false,
+  size: '50px',
+  borderWidth: 7,
 }
 
 export default Loader
