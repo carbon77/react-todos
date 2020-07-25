e4c1037ce650e309d5098193cc608f32b869599a
Igor <zakatov.igor@yandex.ru>
2020-07-25 17:38:35 +0300

    Added fetching folders

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,69 +1,75 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import './Sidebar.sass'
 import SidebarFolderForm from '../SidebarFolderForm/SidebarFolderForm'
 import SidebarItem from '../SidebarItem/SidebarItem'
+import foldersReducer from '../../store/folders.reducer'
+import foldersAPI from '../../api/folders.api'
+import Loader from '../Loader/Loader'
 
-const Sidebar = ({ items }) => {
+const Sidebar = (props) => {
+  const [state, dispatch] = React.useReducer(foldersReducer, { folders: [] })
   const [isFormShowed, setIsFormShowed] = React.useState(false)
+  const [loading, setLoading] = React.useState(false)
+
+  React.useEffect(() => {
+    setLoading(true)
+    foldersAPI.fetchFolders().then((data) => {
+      dispatch({
+        type: 'SET_FOLDERS',
+        payload: {
+          folders: data,
+        },
+      })
+      setLoading(false)
+    })
+  }, [])
 
   return (
     <div className="sidebar">
-      <ul className="sidebar__list">
-        {!!items.length && (
-          <React.Fragment>
-            <SidebarItem
-              to={'/todos'}
-              text={'All todos'}
-              info={'format_list_bulleted'}
-            />
-            <li className="sidebar__divider" />
-          </React.Fragment>
-        )}
-
-        {items.map((item, index) => (
-          <React.Fragment key={index}>
-            {item.divider ? (
-              <li className="sidebar__divider" />
-            ) : (
+      {loading ? (
+        <Loader />
+      ) : (
+        <ul className="sidebar__list">
+          {!!state.folders.length && (
+            <React.Fragment>
               <SidebarItem
-                text={item.text}
-                info={item.icon || item.color}
-                to={`/todos/${item.id}`}
-                actions
+                to={'/todos'}
+                text={'All todos'}
+                info={'format_list_bulleted'}
               />
-            )}
-          </React.Fragment>
-        ))}
-        {!items.length || <li className="sidebar__divider" />}
-        <SidebarItem
-          info={'add'}
-          className="text-muted"
-          text={'Add folder'}
-          onClick={() => setIsFormShowed(true)}
-        />
-        {isFormShowed && (
-          <SidebarFolderForm onClose={() => setIsFormShowed(false)} />
-        )}
-      </ul>
+              <li className="sidebar__divider" />
+            </React.Fragment>
+          )}
+
+          {state.folders.map((folder, index) => (
+            <React.Fragment key={index}>
+              {folder.divider ? (
+                <li className="sidebar__divider" />
+              ) : (
+                <SidebarItem
+                  text={folder.text}
+                  info={folder.icon || folder.color}
+                  to={`/todos/${folder.id}`}
+                  actions
+                />
+              )}
+            </React.Fragment>
+          ))}
+          {!state.folders.length || <li className="sidebar__divider" />}
+          <SidebarItem
+            info={'add'}
+            className="text-muted"
+            text={'Add folder'}
+            onClick={() => setIsFormShowed(true)}
+          />
+          {isFormShowed && (
+            <SidebarFolderForm onClose={() => setIsFormShowed(false)} />
+          )}
+        </ul>
+      )}
     </div>
   )
-}
-
-Sidebar.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      text: PropTypes.string,
-      icon: PropTypes.string,
-      color: PropTypes.string,
-      divider: PropTypes.bool,
-    })
-  ),
-}
-
-Sidebar.defaultProps = {
-  items: [],
 }
 
 export default Sidebar
