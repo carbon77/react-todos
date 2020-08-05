eb91345fab0695175e4740e34654ef009ff9b744
Igor <zakatov.igor@yandex.ru>
2020-08-05 11:15:25 +0300

    Added folders thunks

--- src/store/folders.reducer.js
+++ src/store/folders.reducer.js
@@ -1,3 +1,5 @@
+import foldersAPI from '../api/folders.api'
+
 // Action types
 const SET_FOLDERS = 'SET_FOLDERS'
 const ADD_FOLDER = 'ADD_FOLDER'
@@ -55,23 +57,56 @@ export function setFolders(folders) {
   }
 }
 
-export function addFolder(folder) {
+export function addFolderActionCreator(folder) {
   return {
     type: ADD_FOLDER,
     payload: folder,
   }
 }
 
-export function deleteFolder(id) {
+export function deleteFolderActionCreator(id) {
   return {
     type: DELETE_FOLDER,
     payload: { id },
   }
 }
 
-export function updateFolder(id, options) {
+export function updateFolderActionCreator(id, options) {
   return {
     type: UPDATE_FOLDER,
     payload: { id, options },
+  }
+}
+
+// Thunks
+export function fetchFolders() {
+  return (dispatch) => {
+    return foldersAPI.fetchFolders().then((folders) => {
+      dispatch(setFolders(folders))
+    })
+  }
+}
+
+export function addFolder(folder) {
+  return (dispatch) => {
+    return foldersAPI.createFolder(folder.text, folder.color).then((folder) => {
+      dispatch(addFolderActionCreator(folder))
+    })
+  }
+}
+
+export function deleteFolder(id) {
+  return (dispatch) => {
+    return foldersAPI.deleteFolder(id).then(() => {
+      dispatch(deleteFolderActionCreator(id))
+    })
+  }
+}
+
+export function updateFolder(id, options) {
+  return (dispatch) => {
+    foldersAPI.updateFolder(id, options).then(() => {
+      dispatch(updateFolderActionCreator(id, options))
+    })
   }
 }
