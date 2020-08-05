847fad547686355a75b8c7f5abf2bc1ad8dbfa20
Igor <zakatov.igor@yandex.ru>
2020-08-05 11:14:49 +0300

    Added redux-thunk middleware to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import thunk from 'redux-thunk'
 
 import foldersReducer from './folders.reducer'
 import todosReducer from './todos.reducer'
@@ -10,7 +11,10 @@ const reducers = combineReducers({
 
 const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 export default store
